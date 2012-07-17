f16604d1146808e5b5f2547c66817934c2797cf6
Siavash Askari Nasr <siavash.askari.nasr@gmail.com>
2012-07-18 00:01:16 +0430

    First commit

---
+++ README.md
@@ -0,0 +1,25 @@
+Description
+-----------
+
+ImgViewerPlus(Image Viewer Plus) adds zoom, rotate and light switch capabilities with smooth animations to Firefox image viewer.
+
+Clicking the image icon in ImgViewerPlus toolbar minimizes/maximizes it.
+
+You can change background and light color in preferences.
+
+Shortcut keys:
+
+* `Up, Down, Left, Right` - Movement keys
+* `W` - Zoom In
+* `S` - Zoom Out
+* `D` - Rotate Clockwise
+* `A` - Rotate CounterClockwise
+* `L` - Light Switch
+* `M` - Minimize/Maximize toolbar
+* `X` - Reset Image
+
+Credits
+-------
+
+Logo: [gakuseiSean](http://gakuseisean.deviantart.com/)
+Toolbar icons: [Super Mono Icons by Double-J Design](http://www.doublejdesign.co.uk/products-page/icons/super-mono-icons/)
---
+++ data/js/IVPlus.js
@@ -0,0 +1,356 @@
+if($('head link').attr('href') === 'resource://gre/res/TopLevelImageDocument.css' && $('body').children().length === 1 && $('body').children()[0].tagName.toLowerCase() === 'img') {
+    
+    self.port.emit('send prefs_obj');
+    
+    // Getting preferences
+    let prefs;
+    self.port.on('prefs', function(prefs_obj) {
+        prefs = prefs_obj;
+        // If color preferences are not hex color codes, use the default values
+        color_pattern = /^#(?:[0-9a-f]{3}){1,2}$/i;
+        if(!color_pattern.test(prefs['bg_color'])) prefs['bg_color'] = '#222';
+        if(!color_pattern.test(prefs['light_color'])) prefs['light_color'] = '#f1f1f1';
+        // Setting the preferred background color
+        $('body').css('background-color', prefs['bg_color']);
+    });
+    
+    // Using setTimeout to prevent transition on page load
+    setTimeout(function(){ $('body').css('-moz-transition', 'background-color 1s') }, 100);
+    
+    // Adding necessary elements
+    $('body').addClass('ImgViewerPlusBody');
+    $('body').prepend('<div id="ivp-toolbar" class="pictxt panel">' +
+                      ' <div id="minimize" class="button">Minimize</div>' +
+                      ' <div id="zoom-in" class="button">Zoom In</div>' +
+                      ' <div id="zoom-out" class="button">Zoom out</div>' +
+                      ' <div id="reset-zoom" class="button">Reset Zoom</div>' +
+                      ' <div id="rotate-ccw" class="button">Rotate Counterclockwise</div>' +
+                      ' <div id="rotate-cw" class="button">Rotate Clockwise</div>' +
+                      ' <div id="reset-img" class="button">Reset Image</div>' +
+                      ' <div id="lights" class="button">Turn on/off the lights</div>' +
+                      '</div>'
+                     );
+    $('#ivp-toolbar').after('<div id="navigation-container" class="pictxt">' +
+                            '<div id="navigation" class="panel">' +
+                            '  <div id="nav-up" class="button">Up</div>' +
+                            '  <div id="nav-right" class="button">Right</div>' +
+                            '  <div id="nav-down" class="button">Down</div>' +
+                            '  <div id="nav-left" class="button">Left</div>' +
+                            '  <div id="nav-reset" class="button">Reset Position</div>' +
+                            '</div>' +
+                            '</div>'
+                           );
+
+    $('.panel').hover(
+        function() {
+            $(this).stop(true, true).fadeTo("fast", 1);
+        },
+        function() {
+            $(this).stop(true, true).fadeTo("fast", 0.5);
+        }
+    );
+
+    let img = $('img');
+
+    // Clicking the image resets transform's scale and translate functions
+    img.click(function() { reset_img('scale'); reset_img('translate'); });
+
+    // Resizing the window resets all transform functions
+    $(window).resize(function() { reset_img(''); });
+
+    $('#minimize').click(function() { minimize(); });
+
+    // Scale initialization
+    let scale_num = 1;
+    let scale_range = 0.2;
+
+    $('#zoom-in').click(function() { zoom('in'); });
+    $('#zoom-out').click(function() { zoom('out'); });
+    $('#reset-zoom').click(function() { reset_img('scale'); });
+
+    // Rotate initialization
+    let rotation_degree = 0;
+
+    $('#rotate-ccw').click(function() { rotate('ccw'); });
+    $('#rotate-cw').click(function() { rotate('cw'); });
+    $('#reset-img').click(function() { reset_img(''); });
+
+    // Light initialization
+    let lights = 'off';
+    $('#lights').click(function() { light_switch(); });
+
+    $('#nav-up').click(function() { translate('down'); });
+    $('#nav-right').click(function() { translate('left'); });
+    $('#nav-down').click(function() { translate('up'); });
+    $('#nav-left').click(function() { translate('right'); });
+    $('#nav-reset').click(function() { reset_img('translate'); });
+
+    // Translate initialization
+    let translate_x = 0;
+    let translate_y = 0;
+    let translate_range = 50;
+    let keys = {};
+    let shortcut_keys = [87, 83, 65, 68, 88, 76, 77, 40, 38, 39, 37];
+    
+    // Capturing keys, using "keys" object to capture multiple keys
+    $(document).keydown(function(event) {
+        if(shortcut_keys.indexOf(event.which) > -1) {
+            keys[event.which] = true;
+            process_keys(keys);
+            event.preventDefault();
+        }
+    }).keyup(function(event) {
+        if(shortcut_keys.indexOf(event.which) > -1) keys[event.which] = false;
+    });
+
+    /* Functions */
+    
+    function process_keys(keys) {
+        if(keys[87]) zoom('in');    // W
+        else if(keys[83]) zoom('out');  // S
+        else if(keys[65]) rotate('ccw');    // A
+        else if(keys[68]) rotate('cw'); // D
+        else if(keys[88]) reset_img(''); // X
+        else if(keys[76]) light_switch();   // L
+        else if(keys[77]) minimize();   // M
+        else if(keys[40] && keys[37]) translate('up right');  // Down & Left
+        else if(keys[40] && keys[39]) translate('up left');   // Down & Right
+        else if(keys[38] && keys[37]) translate('down right');   // Up & Left
+        else if(keys[38] && keys[39]) translate('down left');   // Up & Right
+        else if(keys[40]) translate('up'); // Down
+        else if(keys[38]) translate('down');   // Up
+        else if(keys[39]) translate('left');   // Right
+        else if(keys[37]) translate('right');   // Left
+    }
+    
+    function minimize() {
+        $('#navigation').stop(true, true).fadeToggle(0);
+        $('#ivp-toolbar').stop(true, true).animate({ height: $('#ivp-toolbar').css("height") === "38px" ? "310px" : "38px" }, "fast");
+    }
+    
+    function light_switch() {
+        if(lights === 'off') {
+            $('body').css('background', prefs['light_color']);
+            lights = 'on';
+        } else if(lights === 'on') {
+            $('body').css('background', prefs['bg_color']);
+            lights = 'off';
+        }
+    }
+
+    function reset_img(transform_func) {
+        switch(transform_func) {
+            case 'scale':
+                scale_num = 1;
+                break;
+            case 'rotate':
+                reset_rotate();
+                break;
+            case 'translate':
+                translate_x = 0;
+                translate_y = 0;
+                break;
+            default:
+                scale_num = 1;
+                reset_rotate();
+                translate_x = 0;
+                translate_y = 0;
+        }
+        transform('');
+    }
+
+    // "reset_rotate" function does some calculations to determine which way to rotate
+    function reset_rotate() {
+        let remained_deg = Math.abs(rotation_degree)%360;
+        switch(remained_deg) {
+            case 0:
+                break;
+            case 90:
+            case 180:
+                if(rotation_degree > 0) {
+                    rotation_degree -= remained_deg;
+                } else if(rotation_degree < 0) {
+                    rotation_degree += remained_deg;
+                }
+                break;
+            case 270:
+                if(rotation_degree > 0) {
+                    rotation_degree += 90;
+                } else if(rotation_degree < 0) {
+                    rotation_degree -= 90;
+                }
+                break;
+        }
+    }
+
+    function zoom(dir) {
+        if(dir === 'in') scale_num += scale_range;
+        else if(dir === 'out') scale_num -= scale_range;
+        transform('scale');
+    }
+
+    function rotate(dir) {
+        if(dir === 'cw') rotation_degree += 90;
+        else if(dir === 'ccw') rotation_degree -= 90;
+        transform('rotate');
+    }
+
+    // "translate" function determines that in current rotation degree, which direction it should translate
+    function translate(dir) {
+        let remained_deg = Math.abs(rotation_degree)%360;
+        switch(dir) {
+            case 'up':
+                switch(remained_deg) {
+                    case 0:
+                        translate_y -= translate_range;
+                        break;
+                    case 90:
+                        rotation_degree > 0 ? translate_x -= translate_range : translate_x += translate_range;
+                        break;
+                    case 180:
+                        translate_y += translate_range;
+                        break;
+                    case 270:
+                        rotation_degree > 0 ? translate_x += translate_range : translate_x -= translate_range;
+                        break;
+                }
+                break;
+            case 'down':
+                switch(remained_deg) {
+                    case 0:
+                        translate_y += translate_range;
+                        break;
+                    case 90:
+                        rotation_degree > 0 ? translate_x += translate_range : translate_x -= translate_range;
+                        break;
+                    case 180:
+                        translate_y -= translate_range;
+                        break;
+                    case 270:
+                        rotation_degree > 0 ? translate_x -= translate_range : translate_x += translate_range;
+                        break;
+                }
+                break;
+            case 'left':
+                switch(remained_deg) {
+                    case 0:
+                        translate_x -= translate_range;
+                        break;
+                    case 90:
+                        rotation_degree > 0 ? translate_y += translate_range : translate_y -= translate_range;
+                        break;
+                    case 180:
+                        translate_x += translate_range;
+                        break;
+                    case 270:
+                        rotation_degree > 0 ? translate_y -= translate_range : translate_y += translate_range;
+                        break;
+                }
+                break;
+            case 'right':
+                switch(remained_deg) {
+                    case 0:
+                        translate_x += translate_range;
+                        break;
+                    case 90:
+                        rotation_degree > 0 ? translate_y -= translate_range : translate_y += translate_range;
+                        break;
+                    case 180:
+                        translate_x -= translate_range;
+                        break;
+                    case 270:
+                        rotation_degree > 0 ? translate_y += translate_range : translate_y -= translate_range;
+                        break;
+                }
+                break;
+            case 'up left':
+                switch(remained_deg) {
+                    case 0:
+                        translate_y -= translate_range;
+                        translate_x -= translate_range;
+                        break;
+                    case 90:
+                        rotation_degree > 0 ? translate_y += translate_range : translate_y -= translate_range;
+                        rotation_degree > 0 ? translate_x -= translate_range : translate_x += translate_range;
+                        break;
+                    case 180:
+                        translate_y += translate_range;
+                        translate_x += translate_range;
+                        break;
+                    case 270:
+                        rotation_degree > 0 ? translate_y -= translate_range : translate_y += translate_range;
+                        rotation_degree > 0 ? translate_x += translate_range : translate_x -= translate_range;
+                        break;
+                }
+                break;
+            case 'up right':
+                switch(remained_deg) {
+                    case 0:
+                        translate_y -= translate_range;
+                        translate_x += translate_range;
+                        break;
+                    case 90:
+                        rotation_degree > 0 ? translate_y -= translate_range : translate_y += translate_range;
+                        rotation_degree > 0 ? translate_x -= translate_range : translate_x += translate_range;
+                        break;
+                    case 180:
+                        translate_y += translate_range;
+                        translate_x -= translate_range;
+                        break;
+                    case 270:
+                        rotation_degree > 0 ? translate_y += translate_range : translate_y -= translate_range;
+                        rotation_degree > 0 ? translate_x += translate_range : translate_x -= translate_range;
+                        break;
+                }
+                break;
+            case 'down left':
+                switch(remained_deg) {
+                    case 0:
+                        translate_y += translate_range;
+                        translate_x -= translate_range;
+                        break;
+                    case 90:
+                        rotation_degree > 0 ? translate_y += translate_range : translate_y -= translate_range;
+                        rotation_degree > 0 ? translate_x += translate_range : translate_x -= translate_range;
+                        break;
+                    case 180:
+                        translate_y -= translate_range;
+                        translate_x += translate_range;
+                        break;
+                    case 270:
+                        rotation_degree > 0 ? translate_y -= translate_range : translate_y += translate_range;
+                        rotation_degree > 0 ? translate_x -= translate_range : translate_x += translate_range;
+                        break;
+                }
+                break;
+            case 'down right':
+                switch(remained_deg) {
+                    case 0:
+                        translate_y += translate_range;
+                        translate_x += translate_range;
+                        break;
+                    case 90:
+                        rotation_degree > 0 ? translate_y -= translate_range : translate_y += translate_range;
+                        rotation_degree > 0 ? translate_x += translate_range : translate_x -= translate_range;
+                        break;
+                    case 180:
+                        translate_y -= translate_range;
+                        translate_x -= translate_range;
+                        break;
+                    case 270:
+                        rotation_degree > 0 ? translate_y += translate_range : translate_y -= translate_range;
+                        rotation_degree > 0 ? translate_x -= translate_range : translate_x += translate_range;
+                        break;
+                }
+                break;
+        }
+        transform('translate');
+    }
+
+    function transform(transform_func) {
+        if(transform_func === 'scale') {
+            if(scale_num <= 0) scale_num = scale_range;
+        }    
+        img.css('-moz-transform', 'scale(' + scale_num + ') rotate(' + rotation_degree + 'deg) translate(' + translate_x + 'px,' + translate_y + 'px)');
+    }
+}
