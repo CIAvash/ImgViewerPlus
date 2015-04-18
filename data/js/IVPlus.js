322347de34080e97cca0cdba5f4ac25f7c109298
Ignacio Avas <gmail@ignacioavas.com>
2015-04-18 14:30:39 -0300

    Fixed color preferences to work on newer versions of Firefox

--- data/js/IVPlus.js
+++ data/js/IVPlus.js
@@ -2,12 +2,8 @@
 let prefs;
 self.port.on('prefs', function(prefs_obj) {
     prefs = prefs_obj;
-    // If color preferences are not hex color codes, use the default values
-    color_pattern = /^#(?:[0-9a-f]{3}){1,2}$/i;
-    if(!color_pattern.test(prefs['bg_color'])) prefs['bg_color'] = '#222';
-    if(!color_pattern.test(prefs['light_color'])) prefs['light_color'] = '#f1f1f1';
     // Setting the preferred background color
-    $('body').css('background-color', prefs['bg_color']);
+    $('.ImgWrapper').css('background-color', prefs['bg_color']);
 
     if(!prefs['disable_toolbar']) {
 	// Adding necessary elements
@@ -193,10 +189,10 @@ function minimize() {
 
 function light_switch() {
     if(lights === 'off') {
-        $('body').css('background-color', prefs['light_color']);
+        $('.ImgWrapper').css('background-color', prefs['light_color']);
         lights = 'on';
     } else if(lights === 'on') {
-        $('body').css('background-color', prefs['bg_color']);
+        $('.ImgWrapper').css('background-color', prefs['bg_color']);
         lights = 'off';
     }
 }
